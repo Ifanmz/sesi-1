1e691226d7566eea670e58d52782b6d2ab624fdc
Ifanmz <91680459+Ifanmz@users.noreply.github.com>
2022-12-29 09:10:37 +0700

    initial commit

---
+++ src/components/introduction.js
@@ -0,0 +1,10 @@
+function Introduction(props) {
+    return (
+        <>
+            <h1>INTRODUCTION</h1>
+            <p>Hello! my name is: {props.name}</p>
+        </>
+    );
+}
+
+export default Introduction;
---
+++ src/components/profile.js
@@ -0,0 +1,9 @@
+function Profile () {
+    return(
+        <>
+            <h1>IMZ dari sesi 1</h1>
+        </>
+    )
+}
+
+export default Profile;
